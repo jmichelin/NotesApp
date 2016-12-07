1c4192f43df5036e81a83498197c9e83eadcec06
jmichelin <jmichelinphone@gmail.com>
2016-12-07 13:33:57 -0500

    [fix] updated sources in index.html

--- client/notes/notes.js
+++ client/notes/notes.js
@@ -1,6 +1,9 @@
-angular.module('notes-app.notes', [])
-  .factory('viewNoteFactory', function($http) {
-    var getNotes = function() {
+angular.module('notes-app.notes', ['ngQuill'])
+  .config(['ngQuillConfigProvider', function (ngQuillConfigProvider) {
+    ngQuillConfigProvider.set();
+  }])
+  .factory('viewNoteFactory', function ($http) {
+    var getNotes = function () {
       return $http({
         method: "GET",
         url: "api/notes"
@@ -14,45 +17,72 @@ angular.module('notes-app.notes', [])
       getNotes: getNotes
     }
   })
-    .controller('viewNotesCtrl', function($scope, $http, viewNoteFactory) {
-      viewNoteFactory.getNotes().then(function success(data) {
-        $scope.contents = data;
-      })
-      function clicked(data) {
-        console.log("CLICKED DATA", data)
-        $scope.editorData = data;
-      }
+  .controller('viewNotesCtrl', function ($scope, $http, viewNoteFactory) {
+    viewNoteFactory.getNotes().then(function success(data) {
+      $scope.contents = data;
+    })
+    function clicked(data) {
+      console.log("CLICKED DATA", data)
+      $scope.editorData = data;
+    }
+
+    $http({
+      method: "GET",
+      url: "api/notes"
+    }).then(function success(response) {
+      console.log('RESPONSE: ', response);
+      $scope.data = response.data;
+      $scope.data.clicked = clicked;
+      $scope.editorData = '';
+    });
+  })
+  .factory('newNoteFactory', function ($http) {
+    return function postData(data) {
       $http({
-        method: "GET",
-        url: "api/notes"
-      }).then(function success(response) {
+        method: 'POST',
+        url: "api/createNote",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data: data
+      }).then(function (response) {
         console.log('RESPONSE: ', response);
-        $scope.data = response.data;
-        $scope.data.clicked = clicked;
-        $scope.editorData = '';
-      });
-    })
-    .factory('newNoteFactory', function($http) {
-      return function postData(data) {
-        $http({
-          method: 'POST',
-          url: "api/createNote",
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          data: data
-        }).then(function(response) {
-          console.log('RESPONSE: ', response);
-          data.noteName = '';
-          data.noteData = '';
-          return response;
-        })
-      }
-    })
-    .controller('newNoteCtrl', function($scope, $http, newNoteFactory) {
-      var sendData = function(data) {
-        newNoteFactory(data);
+        data.noteName = '';
+        data.noteData = 'type note here';
+        return response;
+      })
+    }
+  })
+  .controller('newNoteCtrl', [
+    '$scope',
+    '$timeout',
+    function($scope, $timeout, newNoteFactory) {
+      $scope.title = 'Quill works';
+      $scope.readOnly = false;
+
+      $timeout(function () {
+        $scope.title += ' awesome!!!';
+      }, 2000);
+
+      $scope.editorCreated = function (editor) {
+        console.log(editor);
+      };
+      $scope.contentChanged = function (editor, html, text) {
+        console.log('editor: ', editor, 'html: ', html, 'text:', text);
       };
       $scope.data = {};
-      $scope.data.sendData = sendData;
-    })
+      $scope.data.sendData = function (data) {
+        newNoteFactory(data);
+      };
+
+     }
+  ]);
+  // .controller('newNoteCtrl', [
+  //   function ($scope, $http, newNoteFactory) {
+  //     var sendData = function (data) {
+  //       newNoteFactory(data);
+  //     };
+  //     $scope.data = {};
+  //     $scope.data.sendData = sendData;
+  //   }
+  // }])
